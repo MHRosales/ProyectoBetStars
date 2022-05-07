0128ddf74a050fcf8c4e436443a1c8e999df3192
MHRosales <20120208@santacecilia.edu.sv>
2022-05-07 15:08:23 -0600

    todos forms

---
+++ ProyectoBetStars/Form76.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoBetStars
+{
+    public partial class Form76 : Form
+    {
+        public Form76()
+        {
+            InitializeComponent();
+        }
+    }
+}
