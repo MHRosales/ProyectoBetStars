38481813729ffdca63d9664c191e73534bf6ca24
MHRosales <20120208@santacecilia.edu.sv>
2022-05-07 15:09:00 -0600

    todos forms

--- ProyectoBetStars/Form100.Designer.cs
+++ ProyectoBetStars/Form100.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form100
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form100";
             this.Text = "Form100";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
